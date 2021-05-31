626ce7df1a79e3d6e8ae92359d6082bc5b970938
Rafael Silveira de Andrade <rafaelsilveirarafa@gmail.com>
2021-05-31 16:55:30 -0300

    adicao do cronometro

---
+++ js/contador_timer.js
@@ -0,0 +1,46 @@
+/* function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = minutes + ":" + seconds;
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+window.onload = function () {
+    var duration = 60 * 5; // Converter para segundos
+        display = document.querySelector('#timer'); // selecionando o timer
+    startTimer(duration, display); // iniciando o timer
+}; */
+
+var target_date = new Date("August 20, 2021").getTime();
+
+var dias, horas, minutos, segundos;
+
+var regressiva = document.getElementById("regressiva");
+
+setInterval(function () {
+
+    var current_date = new Date().getTime();
+    var segundos_f = (target_date - current_date) / 1000;
+
+dias = parseInt(segundos_f / 86400);
+    segundos_f = segundos_f % 86400;
+    
+    horas = parseInt(segundos_f / 3600);
+    segundos_f = segundos_f % 3600;
+    
+    minutos = parseInt(segundos_f / 60);
+    segundos = parseInt(segundos_f % 60);
+
+    document.getElementById('dia').innerHTML = dias;
+document.getElementById('hora').innerHTML = horas;
+document.getElementById('minuto').innerHTML = minutos;
+document.getElementById('segundo').innerHTML = segundos;
+  
+
+}, 1000);
